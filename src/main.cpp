c520b0313636077c94949ec3c8dabbeef10447e0
KekshinMaks <kekshin.maksim@gmail.com>
2026-06-07 13:13:07 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,12 @@
 #include "tree.h"
 
 int main() {
+    std::vector<char> in = {'1','2','3'};
+    PMTree tree(in);
+    
+    std::vector<std::vector<char>> perms = getAllPerms(tree);
+    
+    std::vector<char> result1 = getPerm1(tree, 1);
+    std::vector<char> result2 = getPerm2(tree, 2);
   return 0;
 }
