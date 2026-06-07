c0f0acd126aa45bedef0fb1f904127b254d5b16c
KekshinMaks <kekshin.maksim@gmail.com>
2026-06-07 13:25:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,6 @@
 // Copyright 2022 NNTU-CS
 #include "tree.h"
 #include <algorithm>
-#include <cstdlib>
-#include <fstream>
-#include <iostream>
-#include <locale>
 #include <memory>
 #include <vector>
 
