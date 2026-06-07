471bf1f12a943df1fc877796e7eb115d4c7ddccd
KekshinMaks <kekshin.maksim@gmail.com>
2026-06-07 13:07:28 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,39 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+#include <vector>
+#include <memory>
+
+class PMTree {
+private:
+    struct Node {
+        char value;
+        std::vector<std::shared_ptr<Node>> children;
+        
+        Node(char val) : value(val) {}
+    };
+    
+    std::shared_ptr<Node> root;
+    int factorial(int n) const;
+    void buildTree(std::shared_ptr<Node> node, std::vector<char> remaining);
+    void getAllPermsRecursive(std::shared_ptr<Node> node, 
+                             std::vector<char>& current,
+                             std::vector<std::vector<char>>& result) const;
+    
+public:
+    PMTree(const std::vector<char>& elements);
+    ~PMTree() = default;
+    
+    friend std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
+    friend std::vector<char> getPerm1(const PMTree& tree, int num);
+    friend std::vector<char> getPerm2(const PMTree& tree, int num);
+    
+
+    void printTree() const;
+};
+
+std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
+std::vector<char> getPerm1(const PMTree& tree, int num);
+std::vector<char> getPerm2(const PMTree& tree, int num);
 
 #endif  // INCLUDE_TREE_H_
