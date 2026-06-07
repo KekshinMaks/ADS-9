f035db5c9048ef6eb27aa40c22a603a8555494c3
KekshinMaks <kekshin.maksim@gmail.com>
2026-06-07 13:15:24 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,30 +1,31 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
+
 #include <vector>
 #include <memory>
 
 class PMTree {
-private:
-    struct Node {
-        char value;
-        std::vector<std::shared_ptr<Node>> children;
-        Node(char val) : value(val) {}
-    };
-    
-    std::shared_ptr<Node> root;
-    int factorial(int n) const;
-    void buildTree(std::shared_ptr<Node> node, std::vector<char> remaining);
-    void getAllPermsRecursive(std::shared_ptr<Node> node, 
-                             std::vector<char>& current,
-                             std::vector<std::vector<char>>& result) const;
-    
-public:
-    PMTree(const std::vector<char>& elements);
-    
-    friend std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
-    friend std::vector<char> getPerm1(const PMTree& tree, int num);
-    friend std::vector<char> getPerm2(const PMTree& tree, int num);
+ private:
+  struct Node {
+    char value;
+    std::vector<std::shared_ptr<Node>> children;
+    explicit Node(char val) : value(val) {}
+  };
+
+  std::shared_ptr<Node> root;
+  int factorial(int n) const;
+  void buildTree(std::shared_ptr<Node> node, std::vector<char> remaining);
+  void getAllPermsRecursive(std::shared_ptr<Node> node,
+                            std::vector<char>& current,
+                            std::vector<std::vector<char>>& result) const;
+
+ public:
+  explicit PMTree(const std::vector<char>& elements);
+
+  friend std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
+  friend std::vector<char> getPerm1(const PMTree& tree, int num);
+  friend std::vector<char> getPerm2(const PMTree& tree, int num);
 };
 
 std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
